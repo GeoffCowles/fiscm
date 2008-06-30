f256f9f6d3d9d0da7acf8228c672452048ec11fb
gcowles <gcowles@efa533ab-ad50-0410-8765-1574000216cb>
2008-06-30 21:53:24 +0000

    fiscm main code, initial upload
    git-svn-id: https://fiscm.googlecode.com/svn/trunk@7 efa533ab-ad50-0410-8765-1574000216cb

---
+++ fiscm.f90
@@ -0,0 +1,33 @@
+!=======================================================================
+! Fiscm Main 
+! Copyright:    2008(c)
+!
+! THIS IS A DEMONSTRATION RELEASE. THE AUTHOR(S) MAKE NO REPRESENTATION
+! ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY OTHER PURPOSE. IT IS
+! PROVIDED "AS IS" WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+!
+! THIS ORIGINAL HEADER MUST BE MAINTAINED IN ALL DISTRIBUTED
+! VERSIONS.
+!
+!=======================================================================
+
+Program fiscm
+  use gparms 
+  use mod_igroup
+  use bio
+  type(igroup), allocatable :: igroups(:)
+  integer :: ngroups
+
+  ngroups = 1
+  allocate(igroups(ngroups))
+  igroups(1) = group_(21)
+  igroups(2) = group_(15)
+
+  call init_bio(igroups(1),100)
+  call advance_bio(igroups(1))
+
+
+  deallocate(igroups)
+
+
+End Program fiscm
