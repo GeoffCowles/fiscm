ce70c8686f4e7e742d0e5da2036ffc983a172c5b
gcowles <gcowles@efa533ab-ad50-0410-8765-1574000216cb>
2008-06-30 21:54:06 +0000

    git-svn-id: https://fiscm.googlecode.com/svn/trunk@8 efa533ab-ad50-0410-8765-1574000216cb

--- gparms.f90
+++ gparms.f90
@@ -1,6 +1,6 @@
 !=======================================================================
-! Oscar Global Parameter Class
-! Copyright:    2005(c)
+! Fiscm Global Parameters
+! Copyright:    2008(c)
 !
 ! THIS IS A DEMONSTRATION RELEASE. THE AUTHOR(S) MAKE NO REPRESENTATION
 ! ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY OTHER PURPOSE. IT IS
@@ -9,10 +9,6 @@
 ! THIS ORIGINAL HEADER MUST BE MAINTAINED IN ALL DISTRIBUTED
 ! VERSIONS.
 !
-! Authors:      G. Cowles 
-!               School for Marine Science and Technology     
-!               University of Massachusetts-Dartmouth
-!
 ! Comments:     FISCM Global Parameters
 !=======================================================================
 
@@ -20,19 +16,19 @@ Module Gparms
 
 Implicit None
 
-!single precision kind (used for pV3)
+!single precision kind 
 integer, parameter :: sp = selected_real_kind(6 , 37)
 
 !double precision kind
-integer, parameter :: dp = selected_real_kind(15,307)
+!integer, parameter :: sp = selected_real_kind(15,307)
 
 !double precision constants
-real(dp), parameter :: an8th = 0.125_dp
-real(dp), parameter :: a4th  = 0.250_dp
-real(dp), parameter :: a3rd  = 1.0_dp/3.0_dp
-real(dp), parameter :: ahalf = 0.500_dp
-real(dp), parameter :: zero  = 0.000_dp
-real(dp), parameter :: one   = 1.000_dp
+real(sp), parameter :: an8th = 0.125_sp
+real(sp), parameter :: a4th  = 0.250_sp
+real(sp), parameter :: a3rd  = 1.0_sp/3.0_sp
+real(sp), parameter :: ahalf = 0.500_sp
+real(sp), parameter :: zero  = 0.000_sp
+real(sp), parameter :: one   = 1.000_sp
 
 !----------------------------------------------------------------
 !string length
@@ -55,31 +51,38 @@ integer, parameter  :: cstr = 30
 !     r2d:    convert radians to degrees
 !----------------------------------------------------------------
 
-real(dp), parameter :: pi  = 3.14159265358979312 
-real(dp), parameter :: d2r = pi/180.0_dp
-real(dp), parameter :: r2d = 180.0_dp/pi
+real(sp), parameter :: pi  = 3.14159265358979312 
+real(sp), parameter :: d2r = pi/180.0_sp
+real(sp), parameter :: r2d = 180.0_sp/pi
 
 !----------------------------------------------------------------
 !oceanic parameters
 !    gacc        :  gravitational acceleration   [ms^-2]
 !    omega_earth :  earths rotation rate         [s^-1]
 !----------------------------------------------------------------
-real(dp) :: gacc  = 9.8016_dp !default
-real(dp), parameter :: omega_earth = 7.292e-5
+real(sp) :: gacc  = 9.8016_sp !default
+real(sp), parameter :: omega_earth = 7.292e-5
 
 !----------------------------------------------------------------
 !limiting parameters
 !    CFL_MAX:    max practical CFL
 !----------------------------------------------------------------
-real(dp), parameter :: CFL_MAX = 5.0_dp
+real(sp), parameter :: CFL_MAX = 5.0_sp
 
 !----------------------------------------------------------------
 !large and small numbers
 !    hugenum = largest float
 !    tinynum = smallest float
 !----------------------------------------------------------------
-real(dp), parameter :: hugenum = huge(1.0_dp)
-real(dp), parameter :: tinynum = tiny(1.0_dp)
+real(sp), parameter :: hugenum = huge(1.0_sp)
+real(sp), parameter :: tinynum = tiny(1.0_sp)
 
+!----------------------------------------------------------------
+! control netcdf output of variables
+!    NETCDF_YES:  yes, output
+!    NETCDF_NO:   no, do not output
+!----------------------------------------------------------------
+integer, parameter :: NETCDF_YES = 1
+integer, parameter :: NETCDF_NO  = 0
 
 End Module Gparms
