6d143d0393aff02c9b9d4cd5b4b04e42815b7cef
jessmcn <jessmcn@efa533ab-ad50-0410-8765-1574000216cb>
2008-07-23 19:11:27 +0000

    added param file for C.hyp
    git-svn-id: https://fiscm.googlecode.com/svn/trunk@61 efa533ab-ad50-0410-8765-1574000216cb

--- plotfiscm.m
+++ plotfiscm.m
@@ -1,4 +1,4 @@
-%plotfiscm.m
+%% plotfiscm.m
 %This m-file retrieves the output from the fiscm.nc file ouputted using
 %bio_copepod.f90 and can be used to make plots of the various outputs.
 
@@ -10,11 +10,15 @@ T =nc_varget('fiscm_1.nc', 'T');
 N =nc_varget('fiscm_1.nc', 'N');
 diapause =nc_varget('fiscm_1.nc', 'diapause');
 
+
 figure
 hold on
 plot(time,stage);
-plot(time,pasd);
-title('stage development over time');
+plot(time,pasd, 'm');
+title('Stage development over time for C. finmarchicus at -2 C');
 xlabel('time (days)');
 ylabel('stage');
+legend('iStage', 'PASD', 'Location', 'SouthEast');
 
+%temperature was constant. In degrees C, temperature= (see ans=)
+T(1,1)
