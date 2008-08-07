1982e14a84fbfca75296626bb37a1224c9f287df
gcowles <gcowles@efa533ab-ad50-0410-8765-1574000216cb>
2008-08-07 21:11:03 +0000

    Modified fiscm to work in multidimensional (0,2,3) modeling.  Consists of a setup portion and a main loop over time that makes calls to advection/diffusion , biology, and interpolation of external variables to the particle positions
    git-svn-id: https://fiscm.googlecode.com/svn/trunk@79 efa533ab-ad50-0410-8765-1574000216cb

--- fiscm.f90
+++ fiscm.f90
@@ -1,21 +1,23 @@
 !=======================================================================
 ! Fiscm Main 
-! Copyright:    2008(c)
 !
-! THIS IS A DEMONSTRATION RELEASE. THE AUTHOR(S) MAKE NO REPRESENTATION
-! ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY OTHER PURPOSE. IT IS
-! PROVIDED "AS IS" WITHOUT EXPRESSED OR IMPLIED WARRANTY.
-!
-! THIS ORIGINAL HEADER MUST BE MAINTAINED IN ALL DISTRIBUTED
-! VERSIONS.
-!
+! Description
+!   Main routine for FISCM - Setup and Time Looper 
+!    
+! !REVISION HISTORY:                   
+!  Original author(s): G. Cowles 
 !=======================================================================
 Module fiscm_data
   use gparms
   use mod_igroup
+  use forcing
   implicit none
   type(igroup), allocatable :: igroups(:)
+  character(len=fstr) :: runcontrol 
   real(sp) :: beg_time,end_time
+  real(sp) :: beg_time_days,end_time_days
+  real(sp) :: fbeg,fend
+  real(sp) :: tsmin,tsmax
   real(sp) :: deltaT
   integer  :: Ireport 
   integer  :: sim_direction
@@ -23,11 +25,11 @@ Module fiscm_data
   character(len=fstr) :: forcing_file
 
   Namelist /NML_FISCM/ &
-      beg_time,       & 
-      end_time,       &
-      deltaT,         &
-      ireport,        &
-      Ngroups,        &
+      beg_time_days,   & 
+      end_time_days,   &
+      deltaT,          &
+      ireport,         &
+      Ngroups,         &
       forcing_file
   
 End Module fiscm_data
@@ -36,79 +38,140 @@ Program fiscm
   use gparms 
   use fiscm_data
   use mod_igroup
-  use mod_AD
+  use mod_driver
   use bio
   use output_routines
   use forcing
+  use utilities
   implicit none
   integer :: i,n,its,nits
   real(sp):: t = 0
+  integer :: nvars ,uniqvars
+  character(len=fstr) :: needvars(max_state_vars)
 
-  !!---------------------------------------------------
+  !===================================================================
+  !initialize the model (read namelists, allocate data, set ic's
+  !===================================================================
+
+  !----------------------------------------------------------
   ! read primary simulation data and setup groups
-  !---------------------------------------------------
+  !----------------------------------------------------------
+  fbeg = -1.
+  fend = -1.
   call setup
 
-  !---------------------------------------------------
-  ! initialize groups and data
-  !---------------------------------------------------
+  !----------------------------------------------------------
+  ! initialize biology 
+  !  - set spawning times
+  !  - set initial locations
+  !  - set initial conditions for other states (e.g. length)
+  !----------------------------------------------------------
   do n=1,ngroups
     if(igroups(n)%biology)call init_bio(igroups(n),igroups(n)%Tnind)
   end do
 
-  !---------------------------------------------------
+  !----------------------------------------------------------
   ! define variables for output
-  !---------------------------------------------------
+  !----------------------------------------------------------
   call cdf_out(ngroups,igroups,0,t,NCDO_ADD_STATES)
 
-  !---------------------------------------------------
-  ! setup and simulation summary
-  !---------------------------------------------------
+  !----------------------------------------------------------
+  ! setup connection with forcing 
+  !   determine which forcing vars needed from states
+  !   determine which forcing vars needed for adv/diffusion 
+  !   read in the mesh [ocean model dependent]
+  !   setup frames to store data read from netcdf
+  !----------------------------------------------------------
+  nvars = 0
+  call get_ext_varnames(ngroups,igroups,nvars,needvars)
+  call ocean_model_init(ngroups,igroups,nvars,needvars)
+  call get_unique_strings(nvars,needvars,uniqvars);nvars = uniqvars
+  if(nvars > 0 .and. maxval(igroups%space_dim) > 1)call setup_forcing(nvars,needvars)
+
+  !----------------------------------------------------------
+  ! find elements containing particles 
+  !----------------------------------------------------------
+  call update_element(ngroups,igroups) 
+  if(.not. checkstatus(ngroups,igroups,t))then
+    write(*,*)'all dead'
+    stop
+  endif
+
+  !----------------------------------------------------------
+  ! summarize all states to the screen 
+  !----------------------------------------------------------
   do n=1,ngroups
     call print_group_summary(igroups(n))
   end do
 
-  !---------------------------------------------------
-  ! open input file
-  ! ensure input file contains necessary data
-  !---------------------------------------------------
-  do n=1,ngroups
-    call check_forcing(igroups(n),forcing_file,beg_time,end_time,sim_direction)
-  end do
+  !----------------------------------------------------------
+  ! ensure times (spawning, simulation, forcing)
+  ! are compatible
+  !----------------------------------------------------------
+  call get_spawn_range(ngroups,igroups,tsmin,tsmax)
+  call check_times(beg_time,end_time,tsmin,tsmax,fbeg,fend)
 
-  !---------------------------------------------------
-  ! main loop over time 
-  !---------------------------------------------------
+  !===================================================================
+  ! => begin main loop over time 
+  !===================================================================
+  call drawline("-") ; write(*,*)'Beginning Sim' ; call drawline("-")
+
   t = beg_time
-  nits = (end_time-beg_time)*day_2_sec/deltaT+1
+  nits = (end_time-beg_time)/deltaT+1
   do its=1,nits
 
-    call adv_diff(ngroups,igroups)
+    !---------------------------------------------------------
+    ! update forcing to time t    
+    !---------------------------------------------------------
+    if(maxval(igroups%space_dim) > 1) call update_forcing(t) 
 
-    do n=1,ngroups
-      if(igroups(n)%biology .and. mod(its,igroups(n)%intvl_bio) == 0)then 
-         call advance_bio(igroups(n),t)
-      endif
-    end do
+    !---------------------------------------------------------
+    ! check for activation through spawning 
+    !---------------------------------------------------------
+    call activate(ngroups,igroups,t,sim_direction) 
 
+    !---------------------------------------------------------
+    ! advect and diffuse particles 
+    !---------------------------------------------------------
+    call do_adv_diff(ngroups,igroups,deltaT)
+
+    !---------------------------------------------------------
+    ! interpolate external vars at new particle positions 
+    !---------------------------------------------------------
+    call interp_forcing(ngroups,igroups)
+
+    !---------------------------------------------------------
+    ! advance the biology in time 
+    !---------------------------------------------------------
+    call do_bio(ngroups,igroups,t,its)
+
+    !---------------------------------------------------------
+    ! output the particle states to a NetCDF file 
+    !---------------------------------------------------------
     call cdf_out(ngroups,igroups,its,t,NCDO_OUTPUT)
 
-    if(mod(its,Ireport)==0)then
-    if(.not. checkstatus(ngroups,igroups,t))then
-      write(*,*)'no active particles left in the simulation'
-      write(*,*)'shutting down prematurely'
-      exit
-    endif
+    !---------------------------------------------------------
+    ! report progress to screen 
+    !---------------------------------------------------------
+    if(mod(its-1,Ireport)==0)then
+      if(.not. checkstatus(ngroups,igroups,t))exit
     endif
 
+    !---------------------------------------------------------
+    ! update the time step 
+    !---------------------------------------------------------
     t = t + deltaT
+
   end do
+  !===================================================================
+  ! <=  end main loop over time 
+  !===================================================================
 
-  !---------------------------------------------------
+  !---------------------------------------------------------
   ! cleanup data
-  !---------------------------------------------------
+  !---------------------------------------------------------
+  call drawline("-") ; write(*,*)'Finalizing Sim' ; call drawline("-")
   deallocate(igroups)
-
 
 End Program fiscm
 
@@ -117,33 +180,62 @@ Subroutine setup
   use fiscm_data
   use mod_igroup
   use output_routines
+  use utilities
   implicit none
   logical :: fexist
   integer, parameter :: iunit = 33
   integer :: n,ios
   real(sp) :: uno = 1.0
   real(sp) :: t   = 0.0
+  character(len=fstr) :: buffer
+
+  !--------------------------------------------------------
+  ! get primary control file name from the command line 
+  !--------------------------------------------------------
+  n = iargc()
+  if(n /= 1)then
+    write(*,*)'incorrect usage: '
+    write(*,*)'Usage:'
+    write(*,*)'   fiscm file.nml'
+    write(*,*)'where file.nml is the main parameter file'
+    write(*,*)'stopping'
+    stop
+  endif
+  call getarg(1,buffer)
+  read(buffer,*) runcontrol 
 
   !--------------------------------------------------------
   ! check for existence of primary control file
   !--------------------------------------------------------
-  inquire(file='fiscm.nml',exist=fexist)
+  call drawline("-")
+  write(*,*)'reading main parameters from: ',trim(runcontrol)
+  call drawline("-")
+  inquire(file=trim(runcontrol),exist=fexist)
   if(.not.fexist)then
-    write(*,*)'fatal error: namelist file: fiscm.nml does not exist, stopping...'
+    write(*,*)'fatal error: namelist file: ',trim(runcontrol),' does not exist, stopping...'
     stop 
   endif
-  open(unit=iunit,file='fiscm.nml',form='formatted')
+
+  !--------------------------------------------------------
+  ! open and read global namelist:  nml_fiscm 
+  !--------------------------------------------------------
+  open(unit=iunit,file=trim(runcontrol),form='formatted')
   read(unit=iunit,nml=nml_fiscm,iostat=ios)
   if(ios /= 0)then
-    write(*,*)'fatal error: could not read fiscm namelist from fiscm.nml',ios
+    write(*,*)'fatal error: could not read fiscm namelist from',trim(runcontrol)
     stop
   endif
 
-  !set values, check validity and report
+  !sanity on number of gruops 
   if(ngroups < 1)then
     write(*,*)'Fatal error: number of groups < 1' ; stop 
   endif
 
+  !set begin/end time in seconds
+  beg_time = beg_time_days*day_2_sec
+  end_time = end_time_days*day_2_sec
+
+  !set simulation direction based on order of begin/end times
   if(end_time /= beg_time)then
     sim_direction = sign(uno, end_time-beg_time)
   else
@@ -151,7 +243,14 @@ Subroutine setup
     stop
   endif
 
+  !check for existence and open forcing file (if needed) 
+  if(trim(forcing_file) /= 'NONE' .and. trim(forcing_file) /= 'none')then
+    fbeg = -1.0
+    fend = -1.0
+    call open_forcing_file(trim(forcing_file),fbeg,fend) 
+  endif
 
+  !report
   write(*,*)'begin time:  ',beg_time
   write(*,*)'end time  :  ',end_time
   if(sim_direction ==  1)  write(*,*)'direction:      forward'
@@ -159,14 +258,83 @@ Subroutine setup
   write(*,*)'time step(s) ',deltaT
   write(*,*)'num groups:  ',ngroups
   write(*,*)'Ireport   :  ',Ireport
+  call drawline("-")
 
+  !---------------------------------------------------
   !read and allocate individual groups
+  !---------------------------------------------------
   allocate(igroups(ngroups))
   do n=1,ngroups
     igroups(n) = group_(iunit,n,deltaT)
   end do
+  close(iunit)
+
+  !---------------------------------------------------
+  !add state variables
+  !---------------------------------------------------
+  do n=1,ngroups
+    call group_addstates(igroups(n))  
+  end do
 
   !open output files and assign netcdf ids to each group
   call cdf_out(ngroups,igroups,0,t,NCDO_HEADER)
+  call drawline("-")
+  write(*,*)'setup complete'
+  call drawline("-")
   
 End Subroutine setup
+
+
+!------------------------------------------------------------------
+! check times 
+!------------------------------------------------------------------
+Subroutine check_times(mbeg,mend,smin,smax,fbeg,fend) 
+  use gparms
+  use utilities
+  implicit none
+  real(sp), intent(in) :: mbeg
+  real(sp), intent(in) :: mend 
+  real(sp), intent(in) :: smin
+  real(sp), intent(in) :: smax
+  real(sp), intent(in) :: fbeg
+  real(sp), intent(in) :: fend 
+  !-----------------------------
+  logical istop
+  real(sp) :: tmin,tmax
+
+  call drawline('-')
+  write(*,*)'checking coherency between: '
+  write(*,*)'   spawning times '
+  write(*,*)'   simulation begin/end time'
+  write(*,*)'   model forcing begin/end time (if applicable)'
+  call drawline('.')
+  write(*,*)'time of latest   spawning: ',gettime(int(smax))
+  write(*,*)'time of earliest spawning: ',gettime(int(smin))
+  if(fbeg >= 0.0 .and. fend >= 0.0)then
+    write(*,*)'netcdf forcing start time: ',gettime(int(fbeg))
+    write(*,*)'netcdf forcing end time  : ',gettime(int(fend))
+  endif
+  write(*,*)'model simulation beg time: ',gettime(int(mbeg))
+  write(*,*)'model simulation end time: ',gettime(int(mend))
+
+  istop = .false.
+  tmin = min(mend,mbeg)
+  tmax = max(mend,mbeg)
+
+  !make sure spawning range is contained inside the simulation range
+  if(smin < tmin .or. smin > tmax .or. smax < tmin .or. smax > tmax)then
+    write(*,*)'fatal error: spawning not contained within simulation bounds' 
+    istop = .true. 
+  endif
+  !make sure simulation range is contained within forcing data time range 
+  !(if forcing active)
+  if(fbeg >= 0.0 .and. fend >= 0.0)then
+    if(tmin < fbeg .or. tmin > fend .or. tmax < fbeg .or. tmax > fend)then
+      write(*,*)'fatal error: simulation not contained within forcing bounds' 
+     istop = .true. 
+    endif
+  endif
+  if(istop) stop
+
+End Subroutine check_times
+
